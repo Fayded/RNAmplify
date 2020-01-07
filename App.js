61358872d52de464f5be95389654a916af2eb187
fayded <kevin.d.fay@gmail.com>
2020-01-07 00:41:30 -0500

    graphql api

--- App.js
+++ App.js
@@ -1,55 +1,103 @@
 import React from 'react';
 import {
   SafeAreaView,
+  View,
   StyleSheet,
   Text,
+  TextInput,
+  Button
 } from 'react-native';
 
+// imports from Amplify library
 import { withAuthenticator } from 'aws-amplify-react-native'
-import { Auth } from 'aws-amplify' 
+import { API, graphqlOperation } from 'aws-amplify'
+
+// import the GraphQL query
+import { listRestaurants } from './src/graphql/queries'
+// import the GraphQL mutation
+import { createRestaurant } from './src/graphql/mutations'
+
+// create client ID
+import uuid from 'uuid/v4'
+const CLIENTID = uuid()
+
 import Amplify from 'aws-amplify'
 import config from './aws-exports'
 Amplify.configure(config)
 
 class App extends React.Component {
+  // add additional state to hold form state as well as restaurant data returned from the API
   state = {
-    username: '', password: '', email: ''
+    name: '', description: '', city: '', restaurants: []
   }
-  // event handler
-  onChangeText = (key, value) => {
-    this.setState({ [key]: value })
-  }
+  // execute the query in componentDidMount
   async componentDidMount() {
-    const user = await Auth.currentAuthenticatedUser()
-    console.log('user:', user)
-  }
-  signUp = async() => {
-    const { username, password, email } = this.state
     try {
-      await Auth.signUp({ username, password, attributes: { email }})
+      const restaurantData = await API.graphql(graphqlOperation(listRestaurants))
+      console.log('restaurantData:', restaurantData)
+      this.setState({
+        restaurants: restaurantData.data.listRestaurants.items
+      })
     } catch (err) {
-      console.log('error signing up user...', err)
+      console.log('error fetching restaurants...', err)
     }
   }
-  signOut = () => {
-    Auth.signOut()
-      .then(() => this.props.onStateChange('signedOut'))
-      .catch(err => console.log('err: ', err))
+  // this method calls the API and creates the mutation
+  createRestaurant = async() => {
+    const { name, description, city  } = this.state
+    // store the restaurant data in a variable
+    const restaurant = {
+      name, description, city, clientId: CLIENTID
+    }
+    // perform an optimistic response to update the UI immediately
+    const restaurants = [...this.state.restaurants, restaurant]
+    this.setState({
+      restaurants,
+      name: '', description: '', city: ''
+      })
+    try {
+      // make the API call
+      await API.graphql(graphqlOperation(createRestaurant, {
+        input: restaurant
+      }))
+      console.log('item created!')
+    } catch (err) {
+      console.log('error creating restaurant...', err)
+    }
+  }
+  // change form state then user types into input
+  onChange = (key, value) => {
+    this.setState({ [key]: value })
   }
   render() {
     return (
-      <div>
+      <SafeAreaView style={styles.container}>
         <TextInput
-          placeholder='username'
-          value={this.state.username}
-          style={{ width: 300, height: 50, margin: 5, backgroundColor: "#ddd" }}
-          onChangeText={v => this.onChange('username', v)}
+          style={{ height: 50, margin: 5, backgroundColor: "#ddd" }}
+          onChangeText={v => this.onChange('name', v)}
+          value={this.state.name} placeholder='name'
         />
-        <SafeAreaView style={styles.container}>
-          <Text style={styles.title}>Hello World</Text>
-          <Text onPress={this.signOut}>Sign Out</Text>
-        </SafeAreaView>
-      </div>
+        <TextInput
+          style={{ height: 50, margin: 5, backgroundColor: "#ddd" }}
+          onChangeText={v => this.onChange('description', v)}
+          value={this.state.description} placeholder='description'
+        />
+        <TextInput
+          style={{ height: 50, margin: 5, backgroundColor: "#ddd" }}
+          onChangeText={v => this.onChange('city', v)}
+          value={this.state.city} placeholder='city'
+        />
+        <Button onPress={this.createRestaurant} title='Create Restaurant' />
+        {
+          this.state.restaurants.map((restaurant, index) => (
+            <View key={index} style={styles.item}>
+              <Text style={styles.name}>{restaurant.name}</Text>
+              <Text style={styles.description}>{restaurant.description}</Text>
+              <Text style={styles.city}>{restaurant.city}</Text>
+            </View>
+          ))
+        }
+      </SafeAreaView>
     )
   }
 }
@@ -58,11 +106,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
   },
-  title: {
-    fontSize: 28
-  }
+  item: { padding: 10 },
+  name: { fontSize: 20 },
+  description: { fontWeight: '600', marginTop: 4, color: 'rgba(0, 0, 0, .5)' },
+  city: { marginTop: 4 }
 })
 
-export default withAuthenticator(App);
+export default withAuthenticator(App, { includeGreetings: true });
